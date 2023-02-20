7f21a069b6a0ca5c07c5ce8f8f808f01e78ebc8c
bincoi03 <89581154+bincoi03@users.noreply.github.com>
2023-02-20 11:39:28 +0700

    Notification and bell final

--- src/controller/libraryController.js
+++ src/controller/libraryController.js
@@ -41,7 +41,7 @@ let getLibraryPage = async(req, res) => {
     var [cart] = await pool.execute('select user_id, cart.book_id, books.book_title, books.author, books.cover from cart join books on cart.book_id = books.book_id where user_id = ? and pending = 0', [req.session.user_id]);
     var SortedTemp = Array.from(Booklist).sort();
     Booklist = new Set(SortedTemp);
-    var [notification] = await pool.execute('select (case when l.query = 1 then concat("Borrow book ", b.book_title, " successfully") else concat("Return book ", b.book_title, " successfully") end) as noti, l.day as date_add from log as l join books as b on l.book_id = b.book_id ');
+    var [notification] = await pool.execute('select (case when l.query = 1 then concat("Borrow book ", b.book_title, " successfully") else concat("Return book ", b.book_title, " successfully") end) as noti, l.day as date_add from log as l join books as b on l.book_id = b.book_id order by l.day DESC');
     console.log(notification);
     for (var i = 0; i < notification.length; ++ i){
         var timeStamp = Date.parse(notification[i].date_add);
@@ -199,7 +199,7 @@ let AcceptRequest = async(req, res) => {
         await pool.execute('delete from cart where user_id = ? and book_id = ?', [request[0].user_id, Books[item].book_id]);
         await pool.execute('update books set available = 0 where book_id = ?', [Books[item].book_id]);
         await pool.execute('insert into borrow (user_id, book_id, borrow_date, return_date) values (?, ?, current_date(), date_add(current_date(), INTERVAL 7 DAY))', [request[0].user_id, Books[item].book_id]);
-        await pool.execute('insert into log (user_id, book_id, query, day) values (?,?, 1, current_date())', [request[0].user_id, Books[item].book_id]);
+        await pool.execute('insert into log (user_id, book_id, query, day) values (?,?, 1, now())', [request[0].user_id, Books[item].book_id]);
     }
 
     await pool.execute('delete from request where request_id = ?', [req.params._id]);
@@ -236,7 +236,7 @@ let ReturnBooks = async(req, res) => {
     for (var i = 0; i < Book.length; i++) {
         await pool.execute('update books set available = 1 where book_id = ?', [Book[i]]);
         await pool.execute('delete from borrow where book_id = ?', [Book[i]]);
-        await pool.execute('insert into log (user_id, book_id, query, day) values (?,?, 2, current_date())', [req.params._id, Book[i]]);
+        await pool.execute('insert into log (user_id, book_id, query, day) values (?,?, 2, now())', [req.session.user_id, Book[i]]);
     }
     req.flash('message', 'Return book successfully');
     res.redirect('/return');
